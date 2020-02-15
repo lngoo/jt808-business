c5bbada42773455da5a73f121fb77ce7722c8c66
ligl <ligl@avic-digital.com/avicit>
2020-02-15 22:04:35 +0800

    任务队列add到DB成功

--- src/main/java/com/example/demo/Resovler.java
+++ src/main/java/com/example/demo/Resovler.java
@@ -1,23 +1,7 @@
 package com.example.demo;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.TypeReference;
-import com.ant.msger.base.common.MessageId;
-import com.ant.msger.base.dto.jt808.*;
-import com.ant.msger.base.dto.jt808.basics.Message;
-import com.ant.msger.base.enums.SendType;
-import com.ant.msger.base.message.AbstractBody;
-import com.ant.msger.base.message.AntSendChannelMsg;
-import com.example.demo.cache.Cache;
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.io.xml.StaxDriver;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
